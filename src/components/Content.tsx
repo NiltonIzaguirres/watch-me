7a2a9b7ebfc9fffbc0c2caf5813aa312d202c52b
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2021-08-24 23:33:09 -0300

    Refactor

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,28 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+
+interface ContentProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+}
+
+export function Content({ movies }: ContentProps) {
   // Complete aqui
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+      </div>
+    </main>
+  )
 }
